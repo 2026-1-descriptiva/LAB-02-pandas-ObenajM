bd4d738d991404f5c2d6ebc6686ee5d98968c12a
Obeimar Benavides <obenavides@unal.edu.co>
2026-06-06 22:34:01 -0500

    Inicial commit

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_11():
     """
@@ -22,3 +22,6 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
+    df = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+    result = df.groupby('c0')['c4'].apply(lambda x: ','.join(sorted(x))).reset_index()
+    return result
--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_12():
     """
@@ -22,3 +22,7 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    df = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    df['c5'] = df['c5a'] + ':' + df['c5b'].astype(str)
+    result = df.groupby('c0')['c5'].apply(lambda x: ','.join(sorted(x))).reset_index()
+    return result
